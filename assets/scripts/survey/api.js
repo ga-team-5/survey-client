138f0e6f5da30e3299a38fb454dd4e43bba7dec6
dashakho <dasha.khobo@gmail.com>
2019-11-06 12:59:11 -0500

    Created CRUD for surveys

--- assets/scripts/survey/api.js
+++ assets/scripts/survey/api.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const config = require('../config')
 const store = require('../store')
 
@@ -11,6 +13,77 @@ const surveyIndex = () => {
   })
 }
 
+const createSurvey = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+const getSurveys = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+const updateSurvey = function (formData, target) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys/' + target,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: JSON.stringify(formData),
+    contentType: 'application/json'
+  })
+}
+
+const takeSurveys = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+const deleteSurvey = (target) => {
+  return $.ajax({
+    url: config.apiUrl + '/surveys/' + target,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const getSurveyStats = function (target) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys/' + target + '/stats',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 module.exports = {
-  surveyIndex
+  surveyIndex,
+  createSurvey,
+  getSurveys,
+  updateSurvey,
+  takeSurveys,
+  deleteSurvey,
+  getSurveyStats
 }
