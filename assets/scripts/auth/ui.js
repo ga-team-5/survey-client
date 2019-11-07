8c7b479d04fff1088487e05497c3a3a8b34e40d4
khalsaniwas <honeysaini21@gmail.com>
2019-11-07 15:49:27 -0500

    Fixing alert messages

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -6,24 +6,27 @@ const store = require('../store')
 const surveyIndexTemplate = require('../templates/survey.handlebars')
 
 const signUpSuccess = function (data) {
-  $('#message').text('Signed up successfully')
-  $('#message').removeClass()
-  $('#message').addClass('alert')
-  console.log('signUpSuccess ran. Data is :', data)
+  $('#message2').text('Signed up successfully')
+  $('#message2').css({ opacity: 1 })
+  $('#message2').addClass('alert')
+  $('#message2').animate({ opacity: 0 }, 5000)
+  $('#sign-up').trigger('reset')
 }
 
 const signUpFailure = function (error) {
-  $('#message').text('Error on sign up')
-  $('#message').removeClass()
-  $('#message').addClass('alert')
-  console.error('signUpFailure ran. Error is :', error)
+  $('#message2').text('Error on sign up')
+  $('#message2').addClass('alert')
+  $('#message2').css({ opacity: 1 })
+  $('#message2').animate({ opacity: 0 }, 5000)
+  $('#sign-up').trigger('reset')
 }
 
 const signInSuccess = function (data) {
   $('#message').text('Signed in successfully')
   $('#message').removeClass()
   $('#message').addClass('alert')
-  $('#message').animate({ right: '350px',opacity: 0.2 }, 8000)
+  $('#message').css({ opacity: 1 })
+  $('#message').animate({ opacity: 0 }, 5000)
   $('.sign-up').hide()
   $('.sign-in').hide()
   $('.navbar').removeClass('d-none')
@@ -31,21 +34,18 @@ const signInSuccess = function (data) {
   const surveyHTML = surveyIndexTemplate({surveys: data.surveys})
   $('#all-surveys-container').removeClass('d-none')
   $('#all-surveys-container').html(surveyHTML)
-  console.log('signInSuccess ran. Data is :', data)
 }
 
 const signInFailure = function (error) {
   $('#message2').text('Error on sign in')
   $('#message2').addClass('alert')
   $('#message2').css({ opacity: 1 })
-  $('#message2').animate({ opacity: 0 }, 8000)
-  console.error('signInFailure ran. Error is :', error)
+  $('#message2').animate({ opacity: 0 }, 5000)
 }
 
 const signOutSuccess = function () {
-  $('#message').text('Signed out successfully')
-  $('#message').removeClass()
-  $('#message').addClass('alert')
+  $('#message2').text('Signed out successfully')
+  $('#message2').addClass('alert')
   $('form').trigger('reset')
   $('.sign-up').show()
   $('.sign-in').show()
@@ -53,7 +53,6 @@ const signOutSuccess = function () {
   $('#all-surveys-container').html('')
   $('.navbar').addClass('d-none')
   $('#message').css({ opacity: 1 })
-  console.log('signOutSuccess ran and nothing was returned!')
   store.user = null
 }
 
@@ -61,24 +60,18 @@ const signOutFailure = function (error) {
   $('#message').text('Error on sign out')
   $('#message').removeClass()
   $('#message').addClass('alert')
-  console.error('signOutFailure ran. Error is :', error)
 }
 
 const changePasswordSuccess = function () {
   $('#message').text('Changed password successfully')
   $('#message').removeClass()
   $('#message').addClass('alert')
-  $('#change-pw-form').trigger('reset')
-  $('#change-pw-modal').modal('toggle')
-  console.log('changePasswordSuccess ran and nothing was returned!')
 }
 
-const changePasswordFailure = function (response) {
+const changePasswordFailure = function (error) {
   $('#message').text('Error on change password')
   $('#message').removeClass()
   $('#message').addClass('alert')
-  $('#change-pw-form').trigger('reset')
-  $('#change-pw-modal').modal('toggle')
 }
 
 module.exports = {
--- assets/scripts/survey/ui.js
+++ assets/scripts/survey/ui.js
@@ -4,21 +4,18 @@ const surveyIndexTemplate = require('../templates/survey.handlebars')
 const onVoteSuccess = (response) => {
   $('#message').text('Your vote is counted.')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
   $('#message').addClass('alert')
-  console.log(response)
 }
 
 const onVoteFailure = (response) => {
   $('#message').text('You can only vote once.')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
 
-  console.log(response)
 }
 
 const onVoteIndexSuccess = (response) => {
-  console.log(response)
   const votes = response.votes
   const surveyId = votes[0].survey_id
   const voteObj = votes.reduce((acc, cur) => {
@@ -38,8 +35,6 @@ const onVoteIndexSuccess = (response) => {
     }
     return acc
   }, {op1: 0, op2: 0, op3: 0, op4: 0})
-  console.log(voteObj)
-  console.log(surveyId)
   const form = $('#all-surveys-container').find(`form[data-survey-id='${surveyId}']`)
   $(form).find('.op1-display').html(` | votes: ${voteObj.op1}`)
   $(form).find('.op2-display').html(` | votes: ${voteObj.op2}`)
@@ -48,11 +43,9 @@ const onVoteIndexSuccess = (response) => {
 }
 
 const onVoteIndexFailure = (response) => {
-  console.log(response)
 }
 
 const onSurveyCreateSuccess = (response) => {
-  console.log(response)
   $('#createModalForm').trigger('reset')
   $('#createModal').modal('toggle')
   $('.my-survey').trigger('click')
@@ -63,7 +56,6 @@ const onSurveyCreateSuccess = (response) => {
 }
 
 const onSurveyCreateFailure = (response) => {
-  console.log(response)
 }
 
 const onMySurveySuccess = (response) => {
@@ -89,44 +81,41 @@ const onSurveyIndexSuccess = (response) => {
 const onSurveyIndexFailure = (response) => {
   $('#message').text('Ooops, something went wrong')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
 }
 
 const onMySurveyFailure = (response) => {
   $('#message').text('Ooops, something went wrong')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
 }
 
 const onMySurveyEditSuccess = (response) => {
   $('#message').text('Great! You successfully added your survey')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
   $('#message').addClass('alert')
   $('.my-survey').trigger('click')
-  console.log(response)
 }
 
 const onMySurveyEditFailure = (response) => {
   $('#message').text('Try again')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
 }
 
 const onMySurveyDeleteSuccess = (response) => {
   $('#message').text('You successfully deleted your survey')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
   $('#message').addClass('alert')
-  console.log("hello")
   $('.my-survey').trigger('click')
-  console.log(response)
 }
 
 const onMySurveyDeleteFailure = (response) => {
   $('#message').text('Ooops! Try again')
   $('#message').css({ opacity: 1 })
-  $('#message').animate({ opacity: 0 }, 8000)
+  $('#message').animate({ opacity: 0 }, 5000)
 }
 
 module.exports = {
