cd25a161ca60d38ae0540c69f69fff8b1e583783
Griffin Hood <griffinhood@verizon.net>
2019-11-07 09:30:13 -0500

    Removed unecessary api calls

--- assets/scripts/survey/api.js
+++ assets/scripts/survey/api.js
@@ -24,49 +24,6 @@ const createSurvey = function (formData) {
   })
 }
 
-const updateSurvey = function (formData, target) {
-  return $.ajax({
-    url: config.apiUrl + '/surveys/' + target,
-    method: 'PATCH',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    },
-    data: JSON.stringify(formData),
-    contentType: 'application/json'
-  })
-}
-
-const takeSurveys = function (formData) {
-  return $.ajax({
-    url: config.apiUrl + '/surveys',
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    },
-    data: formData
-  })
-}
-
-const deleteSurvey = (target) => {
-  return $.ajax({
-    url: config.apiUrl + '/surveys/' + target,
-    method: 'DELETE',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-
-const getSurveyStats = function (target) {
-  return $.ajax({
-    url: config.apiUrl + '/surveys/' + target + '/stats',
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-
 const voteCreate = (optionId, surveyId) => {
   return $.ajax({
     url: config.apiUrl + '/votes',
