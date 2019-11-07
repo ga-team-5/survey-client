e5457be6821bc502df2c851f77afab1ea7202664
Griffin Hood <griffinhood@verizon.net>
2019-11-07 10:50:40 -0500

    Display vote results in the survey

--- assets/scripts/survey/ui.js
+++ assets/scripts/survey/ui.js
@@ -18,6 +18,32 @@ const onVoteFailure = (response) => {
 
 const onVoteIndexSuccess = (response) => {
   console.log(response)
+  const votes = response.votes
+  const surveyId = votes[0].survey_id
+  const voteObj = votes.reduce((acc, cur) => {
+    switch (cur.option_id) {
+      case 1:
+        acc.op1 += 1
+        break
+      case 2:
+        acc.op2 += 1
+        break
+      case 3:
+        acc.op3 += 1
+        break
+      case 4:
+        acc.op4 += 1
+        break
+    }
+    return acc
+  }, {op1: 0, op2: 0, op3: 0, op4: 0})
+  console.log(voteObj)
+  console.log(surveyId)
+  const form = $('#all-surveys-container').find(`form[data-survey-id='${surveyId}']`)
+  $(form).find('.op1-display').html(` | votes: ${voteObj.op1}`)
+  $(form).find('.op2-display').html(` | votes: ${voteObj.op2}`)
+  $(form).find('.op3-display').html(` | votes: ${voteObj.op3}`)
+  $(form).find('.op4-display').html(` | votes: ${voteObj.op4}`)
 }
 
 const onVoteIndexFailure = (response) => {
